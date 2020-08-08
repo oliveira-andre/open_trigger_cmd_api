2aa4c529789566728f2a3695c2a7b935739c61af
Andre Oliveira <desmonplays@gmail.com>
2020-08-08 17:43:30 -0300

    feat: initializing route with controller post to sessions

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import SessionsController from './app/controllers/SessionsController';
+
+const routes = new Router();
+
+routes.post('/sessions', SessionsController.store);
+
+export default routes;
