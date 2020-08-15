41969cd09a70526e52790b6f9eeddfe3b1004164
Andre Oliveira <desmonplays@gmail.com>
2020-08-15 02:42:54 -0300

    feat: validating request schema, the user and password

--- src/app/controllers/SessionsController.js
+++ src/app/controllers/SessionsController.js
@@ -1,6 +1,32 @@
+import * as Yup from 'yup';
+
+import User from '../models/User';
+
 class SessionsController {
   async store(req, res) {
-    return res.json({ ok: true })
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+
+    if(!(await schema.isValid(req.body))) {
+      return res.status(422).json({ error: 'Email ou/e Senha estão inválidos ou em branco' });
+    };
+
+    const { email, password } = req.body;
+    const user = User.findOne({ where: { email: email } });
+
+    if (!user) {
+      return res.status(422).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword())) {
+      return res.status(422).json({ error: "Password doesn't match" });
+    }
+
+    return res.json({
+      user: { email }
+    });
   }
 }
 
