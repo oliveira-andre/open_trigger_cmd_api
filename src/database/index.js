b26d267e59111e4021c038a6b29ebd0098071d0a
Andre Oliveira <desmonplays@gmail.com>
2020-08-15 02:44:24 -0300

    feat: checking if association exists

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,10 @@ import Sequelize from 'sequelize';
 
 import dbConfig from '../config/database';
 
+import User from '../models/User';
+
+const models = [User];
+
 class Database {
   constructor() {
     this.init();
@@ -10,6 +14,7 @@ class Database {
   init() {
     this.connection = new Sequelize(dbConfig);
     models.map((model) => model.init(this.connection));
+    models.map((model) => model.associate && model.associate(this.connection.models));
   }
 }
 
