eb50549b516ac3805e57efd675b2bb74a30fd43a
Andre Oliveira <desmonplays@gmail.com>
2020-08-17 17:12:37 -0300

    feat: adding virtual column password, the string column enc_pass and adding a hook to when create, encrypt the password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -14,8 +14,21 @@ User.init({
     type: DataTypes.STRING,
     allowNull: false,
   },
+  password: {
+    type: DataTypes.VIRTUAL,
+    allowNull: false
+  },
+  encrypted_password: {
+    type: DataTypes.STRING,
+  },
   createdAt: DataTypes.DATE,
   updatedAt: DataTypes.DATE,
 }, { sequelize });
+
+User.beforeCreate(async (user) => {
+  if (user.password) {
+    user.encrypted_password = await bcrypt.hash(user.password, 8);
+  }
+});
 
 export default User;
