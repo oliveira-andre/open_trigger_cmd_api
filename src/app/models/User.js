b9effbcfb43c577d8a7742aef3f84855186d6d12
Andre Oliveira <desmonplays@gmail.com>
2020-08-17 14:08:51 -0300

    fix: redo the user model based on sequelize doc

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,20 +1,21 @@
-import bycript from  'bcryptjs';
-import Sequelize, { Model } from 'sequelize';
+import bcrypt from  'bcryptjs';
+import { DataTypes, Model } from 'sequelize';
+
+import sequelize from './index';
 
 class User extends Model {
-  static init(sequlize) {
-    super.init({
-      email: Sequelize.STRING,
-      created_at: Sequelize.DATE,
-      created_at: Sequelize.DATE,
-    }, { sequelize });
-
-    return this;
-  }
-
-  checkPassword(password){
+  checkPassword(password) {
     return bcrypt.compare(password, this.encrypted_password);
   }
-};
+}
+
+User.init({
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: DataTypes.DATE,
+  updatedAt: DataTypes.DATE,
+}, { sequelize });
 
 export default User;
