d2422f6a1a518ba096ee90d12c17f8e23fe035b5
Andre Oliveira <desmonplays@gmail.com>
2020-08-17 12:21:36 -0300

    fix: switching method names from store to create

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import SessionsController from './app/controllers/SessionsController';
+import UsersController from './app/controllers/UsersController';
 
 const routes = new Router();
 
-routes.post('/sessions', SessionsController.store);
+routes.post('/sessions', SessionsController.create);
+routes.post('/users', UsersController.create);
 
 export default routes;
