d132999c3c5c76df611670528129ab7900b4f4f5
Andre Oliveira <desmonplays@gmail.com>
2020-08-18 16:54:21 -0300

    feat: returning message invalid params

--- src/app/validators/api/v1/sessions/create.js
+++ src/app/validators/api/v1/sessions/create.js
@@ -1,6 +1,13 @@
 import  * as Yup from 'yup';
 
 class createSessionValidator { 
+  async validate(bodyParams) {
+    const validParams = await this.validParams(bodyParams);
+    if(!validParams) {
+      return { valid: validParams, message: 'Invalid Params' };
+    }
+  }
+
   async validParams(bodyParams) {
     const schema = Yup.object().shape({
       email: Yup.string().email().required(),
