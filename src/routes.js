5dfb8b91543b5dfc6aca4c4f8a34c39edfd86214
Andre Oliveira <desmonplays@gmail.com>
2020-08-19 03:12:35 -0300

    feat: using middleware to authenticate user

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,17 @@ import { Router } from 'express';
 import {
   SessionsController,
   UsersController,
+  SkillsController,
 } from './app/controllers/api/v1';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionsController.create);
 routes.post('/users', UsersController.create);
+
+routes.use(authMiddleware);
+routes.get('/skills', SkillsController.index);
 
 export default routes;
