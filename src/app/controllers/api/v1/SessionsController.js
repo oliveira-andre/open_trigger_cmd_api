fdc578fa7df793f401408ed1dc3c4ae73c0a556d
Andre Oliveira <desmonplays@gmail.com>
2020-08-19 02:01:27 -0300

    feat: awaiting result of validator, and passing correct env on jwt signature

--- src/app/controllers/api/v1/SessionsController.js
+++ src/app/controllers/api/v1/SessionsController.js
@@ -1,21 +1,25 @@
-import env from 'dotenv/config';
+import 'dotenv/config';
 import jwt from 'jsonwebtoken';
 
+import User from '../../../models/User';
 import {
   createSessionValidator,
 } from '../../../validators/api/v1/sessions';
 
 class SessionsController {
   async create(req, res) {
-    const validateSession = createSessionValidator.validate(req.body);
+    const validateSession = await createSessionValidator.validate(req.body);
 
     if (!validateSession.valid) {
       return res.status(422).json({ error: validateSession.message });
     }
 
+    const { email } = req.body;
+    const { id } = await User.findOne({ where: { email: email } });
+
     return res.json({
       user: { email },
-      token: jwt.sign({ id }, env.SECRET_JWT, {
+      token: jwt.sign({ id }, process.env.secret_jwt, {
         expiresIn: 60 * 60
       })
     });
