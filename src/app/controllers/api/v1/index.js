0cfe24437160a58b56aa6f25bf5715085a866df3
Andre Oliveira <desmonplays@gmail.com>
2020-08-19 03:10:36 -0300

    feat: importing skills controller

--- src/app/controllers/api/v1/index.js
+++ src/app/controllers/api/v1/index.js
@@ -1,7 +1,9 @@
-import SessionsController from './SessionsController';
 import UsersController from './UsersController';
+import SessionsController from './SessionsController';
+import SkillsController from './SkillsController';
 
 export {
   SessionsController,
   UsersController,
+  SkillsController,
 };
