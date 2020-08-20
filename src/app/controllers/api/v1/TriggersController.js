25123ed4c63c300499aec3b9a5a486e5cf048141
Andre Oliveira <desmonplays@gmail.com>
2020-08-20 00:10:45 -0300

    feat: simple return ok true, on create trigger

--- src/app/controllers/api/v1/TriggersController.js
+++ src/app/controllers/api/v1/TriggersController.js
@@ -2,6 +2,10 @@ class TriggersController {
   async index(req, res) {
     res.json({ ok: true });
   }
+
+  async create(req, res) {
+    res.json({ ok: true });
+  }
 }
 
 export default new TriggersController();
