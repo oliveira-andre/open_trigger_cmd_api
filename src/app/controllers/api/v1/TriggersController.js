beb37838e12e5f58ad0570d6a7dd622b36968c1b
Andre Oliveira <desmonplays@gmail.com>
2020-08-20 00:27:43 -0300

    feat: getting return of validator and, if invalid return 422 with error message

--- src/app/controllers/api/v1/TriggersController.js
+++ src/app/controllers/api/v1/TriggersController.js
@@ -1,9 +1,19 @@
+import {
+  createTriggerValidator,
+} from '../../../validators/api/v1/triggers';
+
 class TriggersController {
   async index(req, res) {
     res.json({ ok: true });
   }
 
   async create(req, res) {
+    const validateTrigger = await createTriggerValidator.validate(req.body);
+
+    if (!validateTrigger.valid) {
+      return res.status(422).json({ error: validateTrigger.message });
+    }
+
     res.json({ ok: true });
   }
 }
