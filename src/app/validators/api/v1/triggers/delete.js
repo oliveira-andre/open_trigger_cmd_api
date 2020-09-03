b098b12f853b5e64ebcbd8db124db045b5a9e24c
Andre Oliveira <desmonplays@gmail.com>
2020-09-03 19:19:34 -0300

    feat: checking if trigger exist

--- src/app/validators/api/v1/triggers/delete.js
+++ src/app/validators/api/v1/triggers/delete.js
@@ -1,6 +1,22 @@
+import Trigger from '../../../../models/Trigger';
+
 class deleteTriggerValidator {
-  async validate() {
+  async validate(userId, triggerId) {
+    const triggerExist = await this.triggerExist(userId, triggerId);
+    if(!triggerExist) {
+      return { valid: false, message: 'Trigger not found' };
+    }
+
     return { valid: true };
+  }
+
+  async triggerExist(userId, triggerId) {
+    const trigger =  await Trigger.findOne({ where: {
+      id: triggerId,
+      userId: userId
+    }});
+
+    return trigger;
   }
 }
 
