14d349706ab10f6a4e6b00b292d84a62ae4fd38c
Andre Oliveira <desmonplays@gmail.com>
2020-09-03 19:03:29 -0300

    feat: exporting delete file and exporting

--- src/app/validators/api/v1/triggers/index.js
+++ src/app/validators/api/v1/triggers/index.js
@@ -1,10 +1,13 @@
 import create from './create';
 import update from './update';
+import destroy from './delete';
 
 const createTriggerValidator = create;
 const updateTriggerValidator = update;
+const deleteTriggerValidator = destroy;
 
 export {
   createTriggerValidator,
   updateTriggerValidator,
+  deleteTriggerValidator,
 }
