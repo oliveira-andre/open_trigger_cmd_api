45c0c64dc7f4b1e676aa4270cbfd933e3edf800f
Andre Oliveira <desmonplays@gmail.com>
2020-09-04 14:08:42 -0300

    feat: routing the delete sessions (logout)

--- src/routes.js
+++ src/routes.js
@@ -15,6 +15,8 @@ routes.post('/users', UsersController.create);
 
 routes.use(authMiddleware);
 
+routes.delete('/sessions', SessionsController.delete);
+
 routes.get('/triggers', TriggersController.index);
 routes.post('/triggers', TriggersController.create);
 routes.put('/triggers/:id', TriggersController.update);
