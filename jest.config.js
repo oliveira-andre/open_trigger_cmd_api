a44e8847770a4cf1a1a75ccfd72deafb7bf87dee
Andre Oliveira <desmonplays@gmail.com>
2020-09-07 14:33:46 -0300

    feat: adding transform configuration to load sucrase

--- jest.config.js
+++ jest.config.js
@@ -169,7 +169,7 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: { ".(js|jsx|tx|tsx)": "@sucrase/jest-plugin" },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
