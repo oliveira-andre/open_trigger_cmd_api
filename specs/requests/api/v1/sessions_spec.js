88b9b0dd9942234c70ee6314c07c59790dc8987e
Andre Oliveira <desmonplays@gmail.com>
2020-09-07 18:16:56 -0300

    feat: testing return of session with user not found

--- specs/requests/api/v1/sessions_spec.js
+++ specs/requests/api/v1/sessions_spec.js
@@ -6,29 +6,44 @@ import User from '../../../../src/app/models/User';
 describe('POST /sessions', () => {
   const routePrefix = '/api/v1';
 
+  describe('user not exists', () => {
+    let response = ''
+
+    beforeAll(async () => {
+      response = await request(app)
+        .post(`${routePrefix}/sessions`)
+        .send({ email: 'root@safe.com', password: 'safe123' });
+    });
+
+    it('return error message', async () => {
+      expect(response.body.error).toBe('User not found');
+    });
+
+    it('unprocessable entity status code', async () => {
+      expect(response.status).toBe(422);
+    });
+  });
+
   describe('valid email and password', () => {
     const email = 'root@root.com'
     const password = 'root123'
+    let response = '';
 
     beforeAll(async () => {
       await User.create({
         email: email, password: password
       });
+
+      response = await request(app)
+        .post(`${routePrefix}/sessions`)
+        .send({ email: email, password: password });
     });
 
     it('return status code 201', async () => {
-      const response = await request(app)
-        .post(`${routePrefix}/sessions`)
-        .send({ email: email, password: password });
-
       expect(response.status).toBe(201);
     });
 
     it('return valid jwt token', async () => {
-      const response = await request(app)
-        .post(`${routePrefix}/sessions`)
-        .send({ email: email, password: password });
-
       expect(response.body.token).not.toBeNull();
     });
   });
