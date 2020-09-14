d0cf3a221168fc39193337a2d8f511d7c4cda870
Andre Oliveira <desmonplays@gmail.com>
2020-09-14 16:05:31 -0300

    spec: trying to get triggers with invalid token

--- specs/requests/api/v1/triggers_spec.js
+++ specs/requests/api/v1/triggers_spec.js
@@ -3,6 +3,7 @@ import faker from 'faker';
 
 import app from '../../../../src/app.js';
 import User from '../../../../src/app/models/User';
+import Trigger from '../../../../src/app/models/Trigger';
 
 describe('GET /triggers', () => {
   const routePrefix = '/api/v1';
@@ -21,6 +22,24 @@ describe('GET /triggers', () => {
 
     it('return status code 422', () => {
       expect(response.status).toBe(422);
+    });
+  });
+
+  describe('token invalid or expired', () => {
+    let response = '';
+
+    beforeAll(async () => {
+      response = await request(app)
+        .get(`${routePrefix}/triggers`)
+        .set('Authorization', 'bearer anytoken');
+    });
+
+    it('return error message', () => {
+      expect(response.body.error).toBe('Token invalid');
+    });
+
+    it('return status code 422', () => {
+      expect(response.status).toBe(401);
     });
   });
 });
